42e88c7f7c41e0631ca5c3d32149f805415025cd
dex_tver <ivan.sharapov.05@yandex.ru>
2022-03-12 16:18:10 +0300

    Update

--- src/C.java
+++ src/C.java
@@ -4,7 +4,28 @@ public class C {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt();
-        if (a == 0) System.out.println("no such x");
-        else if (a > 0) System.out.println(String.format("x<%d or x>%d", 2, 3));
+        double x;
+        if (a == 0) {
+            if (b <= 0){
+            System.out.println("no such x");}
+            else{
+                System.out.println("any x");
+            }
+        } else if (a > 0) {
+            x = (double)Math.round(((double) (-b) / a) * 10) / 10; //round
+            if (x < 0) {
+                System.out.println("any x");
+            } else {
+                System.out.printf("x<%s or x>%s%n", -x, x);
+            }
+        } else {
+            x = (double)Math.round(((double) (-b) / a) * 10) / 10;
+            if (x <= 0){
+                System.out.println("no such x");
+            }
+            else{
+                System.out.printf("%s<x<%s%n", -x, x);
+            }
+        }
     }
 }
